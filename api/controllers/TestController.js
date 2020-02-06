c7ab2fb1c7cb02e0d1c7e38df86d8e59339bb6cb
sravan <sravan>
2020-02-06 15:15:42 -0500

    Test API initial implementation

---
+++ api/controllers/TestController.js
@@ -0,0 +1,16 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    test: function(req, res) {
+        res.json({message: "response"});
+    }
+  
+
+};
+
